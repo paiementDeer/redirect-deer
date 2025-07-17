71586097f24baaac438904e3aea5a6b10b9cb4ab
paiementDeer <carole.agencedeximmo@gmail.com>
2025-07-17 11:53:02 +0200

    Create [...slug].js

---
+++ pages/api/[...slug].js
@@ -0,0 +1,13 @@
+export default function handler(req, res) {
+  const { slug } = req.query;
+
+  if (!slug || slug.length < 3) {
+    return res.status(400).send("Lien invalide.");
+  }
+
+  const numeroDossier = slug[2];
+  const redirectUrl = `https://script.google.com/macros/s/AKfycbxyXn22Mnet1TDJgPhhnGAs6q84oN9iGWn8X5KT3b06492tKkof2JzVfM5m4m63rP8/exec?dossier=${numeroDossier}`;
+
+  res.writeHead(302, { Location: redirectUrl });
+  res.end();
+}
